e4ea7d630d1e4cd302fd720133903c57fad8662d
Harsh Gangwal <harshgangwal24@gmail.com>
2026-01-28 17:36:18 +0530

    Add hosted backend URL

--- frontend/src/services/itemService.js
+++ frontend/src/services/itemService.js
@@ -1,5 +1,9 @@
+const backendUrl = import.meta.env.PROD
+  ? import.meta.env.VITE_BACKEND_URL_PRODUCTION
+  : import.meta.env.VITE_BACKEND_URL_LOCAL;
+
 export const fetchItems = async () => {
-  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/items`);
+  const res = await fetch(`${backendUrl}/api/items`);
   if (!res.ok) {
     throw new Error("Failed to fetch items");
   }
